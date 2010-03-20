6022961e267fca3dd2d91fd89cdaabf011efa254
Jason Harwig <jason.harwig@nearinfinity.com>
2010-03-20 16:04:24 -0400

    added gen-sprite and css output

--- test/css_sprite/core_test.clj
+++ test/css_sprite/core_test.clj
@@ -7,17 +7,13 @@
 	
 (testing "get images"
 	(should (> (count (get-images tango-dir)) 0)))
-	
-(testing "combine"
-	(combine-images (get-images tango-dir) :vertical "/Users/jharwig/Desktop/sprite.png"))
 
 (testing "vertical layout"
 	(let [images (get-images tango-dir)
 				layout (:vertical layouts)
 				coord-images (with-coordinates layout images)]
 		(should (= (count coord-images) (count images)))))
-		
-(testing "write css"
-	(let [images (get-images tango-dir)]
-		(write-css images :vertical "~/Desktop/file.css")
-		))
+			
+(testing "gen sprite"
+	(gen-sprite tango-dir "~/Desktop/sprite.png" "~/Desktop/sprite.css"))
+	
